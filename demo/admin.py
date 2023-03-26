10fca8cb1d27db386496003c0c26f5b755e70634
yueyueaa <1327944851@qq.com>
2023-03-26 21:52:13 +0800

    fix: add models

--- demo/admin.py
+++ demo/admin.py
@@ -6,9 +6,12 @@ from demo.models.area.district import District
 from demo.models.area.city import City
 from demo.models.area.province import Province
 from demo.models.area.street import Street
+from demo.models.requirement.requirement import Requirement
+from demo.models.worker.worker import Worker_table
+
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_number','merchant_uid','buyer_id','product_name','product_price','completion_status','order_creation_date']
+    list_display = ['order_number','merchant_id','user_id','order_create_time','order_complete_time','order_type_number','order_status','order_desc']
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -17,6 +20,12 @@ class UserAdmin(admin.ModelAdmin):
 
 class MerchantAdmin(admin.ModelAdmin):
     list_display = ['merchant_id','merchant_name','merchant_password','merchant_address','merchant_status',"service_type","block"]
+
+class RequirementAdmin(admin.ModelAdmin):
+    list_display = ['requirement_id','requirement_name','requirement_value']
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ['worker_id','worker_name','worker_level','worker_tel']
 
 class DistrictAdmin(admin.ModelAdmin):
     list_display = ['code','name','cityCode','provinceCode']
@@ -43,3 +52,5 @@ admin.site.register(District,DistrictAdmin)
 admin.site.register(City,CityAdmin)
 admin.site.register(Province,ProvinceAdmin)
 admin.site.register(Street,StreetAdmin)
+admin.site.register(Requirement,RequirementAdmin)
+admin.site.register(Worker_table,WorkerAdmin)
--- demo/models/requirement/requirement.py
+++ demo/models/requirement/requirement.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Requirement(models.Model):
     requirement_id = models.IntegerField(primary_key=True,verbose_name='需求id')
-    requirement_name = models.CharField(verbose_name='需求名称')
+    requirement_name = models.CharField(max_length=64,verbose_name='需求名称')
     requirement_value = models.IntegerField(verbose_name='需求价格')
