febadb0b93d6e50918b2c5f4c3dabb8d4ca6ab3b
Yarlincksoon <103584768+Yarlincksoon@users.noreply.github.com>
2023-04-07 22:33:38 +0800

    Add files via upload

--- demo/models/user/user_table.py
+++ demo/models/user/user_table.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class User_table(models.Model):
-    user_id = models.IntegerField(primary_key=True, verbose_name='用户ID')
-    user_password = models.CharField(max_length=64, verbose_name='用户密码',null=True)
+    user_id = models.CharField(primary_key=True,max_length=25,verbose_name='用户ID')
     user_name = models.CharField(max_length=64, verbose_name='用户姓名')
+    user_sex = models.IntegerField(verbose_name='用户性别') # 1:男 2:女
     user_tel = models.IntegerField(verbose_name='用户电话')
     user_age = models.IntegerField(verbose_name='用户年龄')
     user_address = models.CharField(max_length=64, verbose_name='用户地址')
-    user_address_code = models.IntegerField(verbose_name='地址ID')
+    user_address_code = models.IntegerField(verbose_name='地址ID',null=True)
     user_birthday = models.DateField(verbose_name='用户生日')
     user_id_card= models.CharField(max_length=18, verbose_name='用户身份证')
-    user_urgency = models.IntegerField(verbose_name='用户紧急联系人')       
+    user_remark= models.CharField(max_length=255, verbose_name='用户备注',null=True)
     
     def __str__(self):
         return str(self.user_name)
