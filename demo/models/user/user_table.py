9ee785479a9c52472432c9e8d5704a22ebbb395e
Yarlincksoon <103584768+Yarlincksoon@users.noreply.github.com>
2023-04-08 16:34:06 +0800

    Update user_table.py

--- demo/models/user/user_table.py
+++ demo/models/user/user_table.py
@@ -5,7 +5,7 @@ class User_table(models.Model):
     user_id = models.CharField(primary_key=True,max_length=25,verbose_name='用户ID')
     user_name = models.CharField(max_length=64, verbose_name='用户姓名')
     user_sex = models.IntegerField(verbose_name='用户性别') # 1:男 2:女
-    user_tel = models.IntegerField(verbose_name='用户电话')
+    user_tel = models.CharField(max_length=11,verbose_name='用户电话')
     user_age = models.IntegerField(verbose_name='用户年龄')
     user_address = models.CharField(max_length=64, verbose_name='用户地址')
     user_address_code = models.IntegerField(verbose_name='地址ID',null=True)
